147cb34d59bc4ef53a4f67f1705d7a0a4641a453
SnehaPotlapally <snehapotlapally100@gmail.com>
2021-01-14 12:37:36 -0500

    Updates

---
+++ precache-manifest.f63c1577100881c387bc327393683fa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dedc7b50463af4cc32e205af17731a62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b26bc7ceaeb648ecef0",
+    "url": "/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "8612ddbc2543af942b56",
+    "url": "/static/css/main.286847d2.chunk.css"
+  },
+  {
+    "revision": "6b26bc7ceaeb648ecef0",
+    "url": "/static/js/2.47624083.chunk.js"
+  },
+  {
+    "revision": "c5bcf5aa1bde18b6cd864a15e05d7878",
+    "url": "/static/js/2.47624083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8612ddbc2543af942b56",
+    "url": "/static/js/main.504b9175.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
